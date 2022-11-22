41432225c4ef6f79ad9b46ccc0a0609b46a140b6
RobsonDev <robson.arcoleze29@gmail.com>
2022-11-22 15:39:10 -0300

    Backend project created

---
+++ backend/src/test/java/com/robsonarcoleze/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsonarcoleze.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
