3fac393b60ea5f048afd668a929650ae495fe82f
RobsonDev <robson.arcoleze29@gmail.com>
2022-11-23 11:38:04 -0300

    H2 Database

---
+++ backend/src/main/java/com/robsonarcoleze/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.robsonarcoleze.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robsonarcoleze.dscatalog.entities.Category;
+import com.robsonarcoleze.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+		
+	}
+}
