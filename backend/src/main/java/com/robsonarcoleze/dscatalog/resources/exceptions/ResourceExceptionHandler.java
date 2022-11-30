45047cd36d00feebdfd9c0c1832d083bdc04c8b0
RobsonDev <robson.arcoleze29@gmail.com>
2022-11-30 09:03:11 -0300

    Delete Category

--- backend/src/main/java/com/robsonarcoleze/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/robsonarcoleze/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.robsonarcoleze.dscatalog.services.exceptions.DataBaseException;
 import com.robsonarcoleze.dscatalog.services.exceptions.ResourceNotFounException;
 
 @ControllerAdvice
@@ -17,13 +18,27 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFounException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFounException e, HttpServletRequest request){
 		
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError erro = new StandardError();
 		erro.setTimestamp(Instant.now());
-		erro.setStatus(HttpStatus.NOT_FOUND.value());
+		erro.setStatus(status.value());
 		erro.setError("Resource not found");
 		erro.setMessage(e.getMessage());
 		erro.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+		return ResponseEntity.status(status).body(erro);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError erro = new StandardError();
+		erro.setTimestamp(Instant.now());
+		erro.setStatus(status.value());
+		erro.setError("Data Base exception");
+		erro.setMessage(e.getMessage());
+		erro.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
 	}
 
 }
