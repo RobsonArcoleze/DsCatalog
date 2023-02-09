b9bb8865a639173341cc5f16de98878f38bd04e4
RobsonDev <robson.arcoleze29@gmail.com>
2023-02-09 10:30:41 -0300

    feat: Busca de Produtos pelo nome
    Implementado Query JPQL filtrando a busca por nome do produto, Requisição feita com encodeURIComponet 'PC%20Gamer', apçicado o trim() na String para desconsiderar os espaços em branco

--- backend/src/main/java/com/robsonarcoleze/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/robsonarcoleze/dscatalog/services/ProductService.java
@@ -31,9 +31,9 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(Long categoryId, Pageable pageable){
+	public Page<ProductDTO> findAllPaged(Long categoryId, String name, Pageable pageable){
 		Optional<Category> category = (categoryId == 0) ? null : categoryRepository.findById(categoryId);
-		Page <Product> list = repository.find(category, pageable);
+		Page <Product> list = repository.find(category, name, pageable);
 		return list.map(x -> new ProductDTO(x));
 	}
 
